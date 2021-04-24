39662d0f034f937c568f3e03d92b0837fe26fddc
JCrosby0 <jcrosby@internode.on.net>
2021-04-24 16:47:11 +0800

    typo

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="menu-container flex flex-col pt-4 pb-4 bg-gray-900 text-white text-right ml-auto w-auto"
+    class="menu-container z-20 flex flex-col pt-4 pb-4 bg-gray-900 text-white text-right ml-auto w-auto"
   >
     <nuxt-link
-      v-for="opt in menuOptoins"
+      v-for="opt in menuOptions"
       :key="opt"
       :to="opt.to"
       class="option pl-4 pr-4 m-0 w-full hover:bg-green-300 hover:text-gray-900"
@@ -26,7 +26,6 @@ export default {
 
 <style>
 .menu-container {
-  z-index: 2;
   width: fit-content;
   backdrop-filter: opacity(0.5) blur(4px);
 }
