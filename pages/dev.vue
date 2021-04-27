53da8cd31d4ae7e2856cf616e1a4338f2418a19e
JCrosby0 <jcrosby@internode.on.net>
2021-04-27 18:07:50 +0800

    oneline logo

--- pages/dev.vue
+++ pages/dev.vue
@@ -172,7 +172,7 @@
           }
         </style>
       </svg>
-      <div class="px-4">
+      <div class="px-8">
         <ul>
           <li>Logo is created with radius = 100px</li>
           <li>let unit length (u) be 40% of the radius</li>
@@ -191,45 +191,226 @@
         </ul>
       </div>
     </div>
-    <div class="option pt-8">
-      <input v-model="showGuidelines" type="checkbox" name="showGuidelines" />
-      <label for="showGuidelines">Show Guidelines</label>
+    <div class="config-container py-8">
+      <div class="option">
+        <input v-model="showGuidelines" type="checkbox" name="showGuidelines" />
+        <label for="showGuidelines">Show Guidelines</label>
+      </div>
+      <div class="option">
+        <label for="lineCapsTop">Line-Caps: Top</label>
+        <select v-model="lineCapsTop" name="lineCapsTop">
+          <option
+            v-for="option in lineCapOptions"
+            :key="option"
+            :value="option"
+          >
+            {{ option }}
+          </option>
+        </select>
+      </div>
+      <div class="option">
+        <label for="lineCapsMid">Line-Caps: Mid</label>
+        <select v-model="lineCapsMid" name="lineCapsMid">
+          <option
+            v-for="option in lineCapOptions.slice(1, 3)"
+            :key="option"
+            :value="option"
+          >
+            {{ option }}
+          </option>
+        </select>
+      </div>
+      <div class="option">
+        <label for="crosbyWeight">Font Weight: Crosby</label>
+        <select v-model="crosbyWeight" name="crosbyWeight">
+          <option v-for="option in fontWeights" :key="option" :value="option">
+            {{ option }}
+          </option>
+        </select>
+      </div>
+      <div class="option">
+        <label for="solutionsWeight">Font Weight: Solutions</label>
+        <select v-model="solutionsWeight" name="solutionsWeight">
+          <option v-for="option in fontWeights" :key="option" :value="option">
+            {{ option }}
+          </option>
+        </select>
+      </div>
     </div>
-    <div class="option">
-      <label for="lineCapsTop">Line-Caps: Top</label>
-      <select v-model="lineCapsTop" name="lineCapsTop">
-        <option v-for="option in lineCapOptions" :key="option" :value="option">
-          {{ option }}
-        </option>
-      </select>
-    </div>
-    <div class="option">
-      <label for="lineCapsMid">Line-Caps: Mid</label>
-      <select v-model="lineCapsMid" name="lineCapsMid">
-        <option
-          v-for="option in lineCapOptions.slice(1, 3)"
-          :key="option"
-          :value="option"
+    <div class="svgcontainer2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMidYMid meet"
+        overflow="auto"
+        :viewBox="`60 -70 ${totalWidthOL - 170} 140`"
+        height="150px"
+        :class="{ svgBorder: showGuidelines }"
+      >
+        <!-- <circle r="100" stroke="#f00" stroke-width="5px" fill="none" /> -->
+        <g
+          v-show="showGuidelines"
+          id="guidelines"
+          stroke-thickness="1px"
+          stroke-dasharray="5"
+          stroke="red"
         >
-          {{ option }}
-        </option>
-      </select>
-    </div>
-    <div class="option">
-      <label for="crosbyWeight">Font Weight: Crosby</label>
-      <select v-model="crosbyWeight" name="crosbyWeight">
-        <option v-for="option in fontWeights" :key="option" :value="option">
-          {{ option }}
-        </option>
-      </select>
-    </div>
-    <div class="option">
-      <label for="solutionsWeight">Font Weight: Solutions</label>
-      <select v-model="solutionsWeight" name="solutionsWeight">
-        <option v-for="option in fontWeights" :key="option" :value="option">
-          {{ option }}
-        </option>
-      </select>
+          <!-- horizontal guidelines -->
+          <path
+            v-for="gy in [-70, -50, -40, 0, 40, 50, 70]"
+            :key="'guideline-y_' + gy"
+            :d="`M -100 ${gy} L ${totalWidthOL} ${gy}`"
+          />
+          <!-- vertical guidelines -->
+          <path
+            v-for="gx in [
+              x.startText - 120,
+              x.startText - 20,
+              x.startText,
+              x.endText,
+              x.endDoubleText,
+            ]"
+            :key="'guideline-x_' + gx"
+            :d="`M ${gx} 100 L ${gx} -100`"
+          />
+        </g>
+        <g
+          id="logo"
+          stroke-width="20px"
+          stroke="#333"
+          fill="none"
+          :stroke-linecap="lineCapsTop"
+          stroke-linejoin="round"
+        >
+          <!-- C Stroke and Fill -->
+          <path
+            id="large-c"
+            :d="`M 0 -${radius}
+             A ${radius} ${radius} 0 1 0 0 ${radius}`"
+            fill="#10b981"
+          />
+          <!-- Bot S Fill -->
+          <path
+            id="bot-s-fill"
+            :d="`M 0 ${radius}
+             A ${radius} ${radius} 0 0 0 ${radius} 0
+             L 0 0`"
+            stroke="none"
+            fill="#6ee7b7"
+          />
+          <!-- Top S Fill -->
+          <path
+            id="topSFill"
+            :d="`M 0 0
+             A ${radius} ${radius} 0 0 1 ${radius} -${radius}
+             L ${radius} 0`"
+            stroke="none"
+            fill="#6ee7b7"
+          />
+          <!-- Bot S Stroke -->
+          <path
+            id="bot-s-stroke"
+            :d="`M 0 ${radius}
+             A ${radius} ${radius} 0 0 0 ${radius} 0`"
+            :stroke-linecap="lineCapsMid"
+          />
+          <!-- Top S Stroke-->
+          <path
+            id="topSStroke"
+            :d="`M 0 0
+             A ${radius} ${radius} 0 0 1 ${radius} -${radius}`"
+          />
+          <!-- Stroke -->
+          <path
+            id="stroke"
+            :d="`M 0 -${radius} L 0 0`"
+            :stroke-linecap="lineCapsMid"
+          />
+        </g>
+        <!-- Text -->
+        <g id="text" color="#333">
+          <!-- Crosby -->
+          <text
+            id="crosby"
+            :x="x.startText - 5"
+            y="40"
+            :textLength="textLength"
+            :style="{ fontWeight: crosbyWeight }"
+          >
+            crosby
+          </text>
+          <!-- textLength="700" -->
+          <!-- Solutions -->
+          <text
+            id="solutionsOL"
+            :x="x.endText"
+            y="40"
+            :style="{ fontWeight: solutionsWeight }"
+            :textLength="textLengthOL"
+          >
+            solutions
+          </text>
+        </g>
+        <style>
+          #text {
+            font-family: 'Montserrat', sans-serif;
+          }
+
+          #crosby {
+            font-size: 149px;
+            border: 1px green dashed;
+          }
+
+          #solutionsOL {
+            font-variant: small-caps;
+            font-size: 160px;
+          }
+
+          #stroke {
+            animation-name: rotate-stroke;
+            animation-duration: 1s;
+            animation-timing-function: linear;
+            animation-iteration-count: 1;
+            animation-fill-mode: forwards;
+          }
+
+          <!-- #topSFill {
+            animation-name rotate-arc;
+            animation-delay: 1s;
+            animation-duration: 1s;
+            animation-timing-function: linear;
+            animation-iteration-count: 1;
+            animation-fill-mode: forwards;
+          } -->
+          #topSStroke {
+            animation-name rotate-arc;
+            animation-delay: 1s;
+            animation-duration: 1s;
+            animation-timing-function: linear;
+            animation-iteration-count: 1;
+            animation-fill-mode: forwards;
+          }
+
+          @keyframes rotate-stroke {
+            0% {
+              transform: rotate(360deg)
+            }
+
+            100% {
+              transform: rotate(90deg)
+            }
+          }
+
+          @keyframes rotate-arc {
+            0% {
+              transform: rotate(360deg);
+            }
+
+            100% {
+              transform: rotate(90deg);
+            }
+          }
+        </style>
+      </svg>
     </div>
   </div>
 </template>
@@ -263,6 +444,13 @@ export default {
         endText: parseInt(
           -100 + this.c.logoSize + this.c.space + this.textLength
         ),
+        endDoubleText: parseInt(
+          -100 +
+            this.c.logoSize +
+            this.c.space +
+            this.textLength +
+            this.textLengthOL
+        ),
       }
     },
     netLogo() {
@@ -277,8 +465,20 @@ export default {
     textLength() {
       return 465
     },
+    textLengthOL() {
+      return this.textLength * 1.2
+    },
     totalWidth() {
       return this.c.logoSize + this.c.space + this.textLength + 16
+    },
+    totalWidthOL() {
+      return (
+        this.c.logoSize +
+        this.c.space +
+        this.textLength +
+        this.textLengthOL +
+        16
+      )
     },
     totalHeight() {
       return this.c.logoSize + this.c.lineThickness
@@ -297,7 +497,11 @@ svg {
 .svgBorder {
   border: green 1px dashed;
 }
-
+/* oneline modification */
+#logo {
+  transform-origin: 0 0;
+  transform: scale(0.5) translateX(220px);
+}
 /* montserrat-100 - latin */
 @font-face {
   font-family: 'Montserrat';
