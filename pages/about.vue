8603c5381e8ed61d3abf38d68cadd1e90bbfb3f5
JCrosby0 <jcrosby@internode.on.net>
2021-05-03 14:16:58 +0800

    add cta to about & solutions

--- pages/about.vue
+++ pages/about.vue
@@ -2,17 +2,22 @@
   <Container>
     <h1 class="text-3xl pb-4">About</h1>
     <Accordion :content="aboutItems" />
+    <div class="text-center mx-auto relative mt-16 mb-16">
+      <CallToAction />
+    </div>
   </Container>
 </template>
 
 <script>
 import Container from '~/components/container'
 import Accordion from '~/components/accordion'
+import CallToAction from '~/components/callToAction'
 export default {
   name: 'About',
   components: {
     Container,
     Accordion,
+    CallToAction,
   },
   async asyncData({ $content }) {
     const aboutItems = await $content('json').where({ slug: 'about' }).fetch()
--- pages/solutions.vue
+++ pages/solutions.vue
@@ -2,17 +2,22 @@
   <Container>
     <h1 class="text-3xl pb-4">Solutions</h1>
     <Accordion :content="solutions" />
+    <div class="text-center mx-auto relative mt-16 mb-16">
+      <CallToAction />
+    </div>
   </Container>
 </template>
 
 <script>
 import Container from '~/components/container'
 import Accordion from '~/components/accordion'
+import CallToAction from '~/components/callToAction'
 export default {
   name: 'Solutions',
   components: {
     Container,
     Accordion,
+    CallToAction,
   },
   async asyncData({ $content }) {
     const solutions = await $content('json')
