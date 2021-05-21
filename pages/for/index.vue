4c8bcd49dcd4ec2823e16ce437e0b7923f0203c9
JCrosby0 <jcrosby@internode.on.net>
2021-05-21 17:22:39 +0800

    slug not working...?

---
+++ pages/for/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <Container>
+    <div>Nothing here.</div>
+  </Container>
+</template>
+
+<script>
+import Container from '~/components/container'
+export default {
+  name: 'Articles',
+  components: {
+    Container,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+.article-preview {
+  text-decoration: none;
+}
+</style>
